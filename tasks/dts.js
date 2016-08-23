d912715225786bb73773a84942182ee3b55d1604
Justin McMahon <Justin@Justins-MacBook-Pro.local>
2016-08-22 20:41:46 -0700

    Adjust build process to copy package.json from root

--- tasks/dts.js
+++ tasks/dts.js
@@ -12,13 +12,19 @@ module.exports = (gulp) =>
 
 	gulp.task('clean:dist', () =>
 	{
-		return del(['dist/**/*', '!dist/package.json', '!dist/tsconfig.json']);
+		return del(['dist/**/*', '!dist/tsconfig.json']);
 	});
 
 	gulp.task('build:readme', () =>
 	{
 		return gulp.src('ReadMe.md')
 			.pipe(gulp.dest('dist'));
+	});
+
+	gulp.task('build:pkg', () =>
+	{
+		return gulp.src('package.json')
+			.pipe(gulp.dest('dist'))
 	});
 
 	let tsFilesGlob = ((c) =>
@@ -78,6 +84,6 @@ module.exports = (gulp) =>
 
 	// run clean:uom task, then run update-tsconfig and gen-def in parallel, then run _build
 	gulp.task('dts', 'Compiles all TypeScript source files and updates module references',
-		gulpSequence('clean:dist', '_build', 'npm-lint', 'build:readme'));
+		gulpSequence('clean:dist', '_build', 'npm-lint', 'build:readme', 'build:pkg'));
 };
 
